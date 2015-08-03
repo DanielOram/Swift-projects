4700c0b515464b03b5968a27ff08f325f6f32cef
Danoram <danoram1@xtra.co.nz>
2015-08-03 22:56:48 +1200

    Commit MVP of Tuku Framework
    This is the Tuku Framework project in its minimal working form.
    supports http GET, POST, and PUT requests.

---
+++ WebResponseApp/WebResponseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebResponseApp
+//
+//  Created by Daniel Oram on 3/08/15.
+//  Copyright (c) 2015 Danoram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
