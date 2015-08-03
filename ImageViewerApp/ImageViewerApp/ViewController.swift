cdf6e2de308a8c8cea4eee68e60b39e0dd775078
Danoram <danoram1@xtra.co.nz>
2015-08-04 09:19:54 +1200

    Commit empty Xcode project
    new App that makes image search queries and displays results

---
+++ ImageViewerApp/ImageViewerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageViewerApp
+//
+//  Created by Daniel Oram on 4/08/15.
+//  Copyright © 2015 Danoram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ImageViewerApp/ImageViewerAppTests/ImageViewerAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImageViewerAppTests.swift
+//  ImageViewerAppTests
+//
+//  Created by Daniel Oram on 4/08/15.
+//  Copyright © 2015 Danoram. All rights reserved.
+//
+
+import XCTest
+@testable import ImageViewerApp
+
+class ImageViewerAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
